9c4a956d138f0568fcf136f88b0dd13cd37289a9
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-04-03 18:14:26 +0100

    Stretch: Return Users From Same Department As Request

--- users/users-router.js
+++ users/users-router.js
@@ -11,15 +11,20 @@ const Users = knex(knexConfig.development)('users');
 router.use(express.json());
 
 router.get('/', authRequired, (req, res) => {
-  Users.select('id', 'username', 'department')
-  .then(usersArray => {
-    res.json(usersArray);
-  })
-  .catch(err => {
-    res.status(500).json({
-      message: `Failed to get users ${err}`
+  const requestUserInfo = req.decodedToken;
+  const getUsersInDepartment = (department) => {
+    return Users.select('id', 'username', 'department').where({ department });
+  }
+
+  getUsersInDepartment(requestUserInfo.department)
+    .then(usersArray => {
+      res.json(usersArray);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: `Failed to get users ${err}`
+      });
     });
-  });
 });
 
 module.exports = router;
