2191292a28b8922fe7ebe2809d0331beb7db9f71
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-04-03 17:15:41 +0100

    Implement Register Endpoint

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,10 +1,37 @@
 const express = require('express');
+const knex = require('knex');
+const jwt = require('jsonwebtoken');
+
+const knexConfig = require('../knexfile');
+
+const Users = knex(knexConfig.development)('users');
 const router = express.Router();
 
 router.use(express.json());
 
 router.post('/register', (req, res) => {
-  res.send(`Registered up as ${req.body.username}`);
+  const { username, password, department } = req.body;
+  const requestBodyComplete = !!(username && password && department);
+
+  if (requestBodyComplete) {
+    Users.insert({ username, password, department })
+      .then(success => {
+        res.json({
+          message: `Registered as ${req.body.username}`
+        })
+      })
+      .catch(err => {
+        res.status(500).json({
+          message: `Failed to create user ${req.body.username}. Try again.`,
+          error: `${err}`
+        });
+      });
+  }
+  else {
+    res.status(400).json({
+      message: "Please provide a username, password & department"
+    })
+  }
 });
 
 router.post('/login', (req, res) => {
