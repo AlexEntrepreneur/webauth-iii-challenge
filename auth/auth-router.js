de83a185101ba1a0778c037ca9fe60128c9cd961
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-04-03 17:47:13 +0100

    Implement Login Endpoint Logic (With Token)

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const knex = require('knex');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 const knexConfig = require('../knexfile');
 
@@ -37,8 +38,66 @@ router.post('/register', (req, res) => {
   }
 });
 
+function makeTokenFromUser(user) {
+  const payload = {
+    user_id: user.id,
+    username: user.username,
+    department: user.department
+  }
+
+  const options = {
+    expiresIn: '1d'
+  }
+
+  const token = jwt.sign(payload, 'Temporary Secret For Testing', options);
+
+
+  return token;
+}
+
 router.post('/login', (req, res) => {
-  res.send(`Logged in as ${req.body.username}`);
+  const { username, password } = req.body;
+  const requestBodyComplete = !!(username && password);
+
+  if (requestBodyComplete) {
+    Users.select('*').where({ username }).first()
+      .then(userObj => {
+        if (userObj) {
+          return userObj;
+        }
+        else {
+          throw 'Invalid credentials. Try again.';
+        }
+      })
+      .then(userObj => {
+        const passwordIsCorrect = bcrypt.compareSync(password, userObj.password);
+        if (passwordIsCorrect) {
+          const token = makeTokenFromUser(userObj);
+          res.json({
+            token,
+            message: `Welcome ${userObj.username}`
+          });
+        }
+        else {
+          throw 'Bad credentials. Try again.';
+        }
+      })
+      .catch(err => {
+        res.status(404).json({
+          message: err
+        });
+      })
+      .catch(err => {
+        res.status(500).json({
+          message: `failed to get users ${err}`
+        });
+      });
+  }
+  else {
+    res.status(400).json({
+      message: "Please provide a username, password & department"
+    })
+  }
 });
 
 module.exports = router;
